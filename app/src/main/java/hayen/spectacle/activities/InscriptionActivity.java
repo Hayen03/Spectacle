197e6b4cb15a95946f8a344812cdfc369249432e
Hayen03 <hayen003@gmail.com>
2018-03-25 13:53:49 -0400

    interface inscription

---
+++ app/src/main/java/hayen/spectacle/activities/InscriptionActivity.java
@@ -0,0 +1,15 @@
+package hayen.spectacle.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hayen.spectacle.R;
+
+public class InscriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscription);
+    }
+}
