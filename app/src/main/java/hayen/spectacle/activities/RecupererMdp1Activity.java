8ae2b9337d506ebf7ca3fcb76a0557767558ea17
Hayen03 <hayen003@gmail.com>
2018-03-25 13:29:26 -0400

    interface mdp oublie

--- app/src/main/java/hayen/spectacle/activities/RecupererMdp1Activity.java
+++ app/src/main/java/hayen/spectacle/activities/RecupererMdp1Activity.java
@@ -1,13 +1,15 @@
-package hayen.spectacle;
+package hayen.spectacle.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import hayen.spectacle.R;
+
+public class RecupererMdp1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_recuperer_mdp1);
     }
 }
