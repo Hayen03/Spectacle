5299fe22579f5ad9a4221dd434efcbbc43be72fa
Hayen03 <hayen003@gmail.com>
2018-03-26 14:14:04 -0400

    corriger le bug avec les fragments

--- app/src/main/java/hayen/spectacle/activities/CalendrierActivity.java
+++ app/src/main/java/hayen/spectacle/activities/CalendrierActivity.java
@@ -1,8 +1,11 @@
 package hayen.spectacle.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.ActionBar;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,10 +15,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import hayen.spectacle.R;
+import hayen.spectacle.fragments.CalendrierFragment;
+import hayen.spectacle.fragments.InfoFragment;
+import hayen.spectacle.fragments.ProfilFragment;
+import hayen.spectacle.fragments.RechercheFragment;
+import hayen.spectacle.fragments.ReservationFragment;
 
-public class CalendrierActivity extends AppCompatActivity {
+public class CalendrierActivity
+        extends     AppCompatActivity
+        implements  CalendrierFragment.OnFragmentInteractionListener,
+                    InfoFragment.OnFragmentInteractionListener,
+                    ProfilFragment.OnFragmentInteractionListener,
+                    RechercheFragment.OnFragmentInteractionListener,
+                    ReservationFragment.OnFragmentInteractionListener,
+                    NavigationView.OnNavigationItemSelectedListener
+{
 
     private DrawerLayout drawer;
+
+    private State fragState = null;
+    private Fragment currFrag = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,42 +44,22 @@ public class CalendrierActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        if (savedInstanceState == null)
+            currFrag = loadFragment(State.calendrier);
+
         drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         NavigationView nav = findViewById(R.id.nav_view);
-        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                switch (id) { // pour plus tard
-                    default:
-                        break;
-                }
-                item.setChecked(true);
-                drawer.closeDrawers();
-                return true;
-            }
-        });
+        nav.setNavigationItemSelectedListener(this);
 
 
 //        ActionBar actionBar = getSupportActionBar();
 //        actionBar.setDisplayHomeAsUpEnabled(true);
 //        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
     }
-
-/*    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                drawer.openDrawer(GravityCompat.START);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    */
 
     @Override
     public void onBackPressed() {
@@ -72,4 +71,70 @@ public class CalendrierActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {}
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) { // pour plus tard
+            case R.id.nav_calendrier:
+                loadFragment(State.calendrier);
+                break;
+            case R.id.nav_reservation:
+                loadFragment(State.reservation);
+                break;
+            case R.id.nav_recherche:
+                loadFragment(State.recherche);
+                break;
+            case R.id.nav_profil:
+                loadFragment(State.profil);
+                break;
+            case R.id.nav_info:
+                loadFragment(State.info);
+                break;
+            case R.id.nav_deconnexion:
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+            default:
+                break;
+        }
+        item.setChecked(true);
+        drawer.closeDrawers();
+        return true;
+    }
+
+    private Fragment loadFragment(State frag){
+        if (fragState != frag) {
+            Fragment fragment = null;
+            Class fragmentClass = null;
+            fragmentClass = frag.fragClass;
+            try {
+                fragment = (Fragment) fragmentClass.newInstance();
+                nothing();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.fragcontent, fragment).commit();
+            return fragment;
+        }
+        return currFrag;
+    }
+
+    private enum State {
+        calendrier(CalendrierFragment.class),
+        reservation(ReservationFragment.class),
+        recherche(RechercheFragment.class),
+        profil(ProfilFragment.class),
+        info(InfoFragment.class);
+
+        public final Class fragClass;
+        State(Class fc){
+            fragClass = fc;
+        }
+    }
+
+    void nothing(){}
 }
